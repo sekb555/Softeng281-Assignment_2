05c8cde2156991969d476af92bfc1f6538b36db7
Shrirang.E <shrirang.ekbote04@gmail.com>
2024-05-03 20:20:00 +1200

    printing the number of the round that is being started when play is invoked

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,24 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  protected String playerName;
+  protected Choice choice;
+  protected Difficulty difficulty;
+  protected int noOfGames = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    this.playerName = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
+    this.choice = choice;
+    this.difficulty = difficulty;
+    noOfGames += 1;
+
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(noOfGames));
+  }
 
   public void endGame() {}
 
