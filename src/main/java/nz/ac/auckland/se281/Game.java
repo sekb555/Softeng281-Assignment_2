5e6e4ee9d99f3e773f2a38f4609af989d4b11f40
Shrirang.E <shrirang.ekbote04@gmail.com>
2024-05-03 20:30:35 +1200

    asking for player to imput number of fingers

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -23,6 +23,7 @@ public class Game {
 
   public void play() {
     MessageCli.START_ROUND.printMessage(String.valueOf(noOfGames));
+    MessageCli.ASK_INPUT.printMessage();
   }
 
   public void endGame() {}
