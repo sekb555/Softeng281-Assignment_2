968d02752137ddb6139eafc162eac9345db676ca
Shrirang.E <shrirang.ekbote04@gmail.com>
2024-05-04 10:43:03 +1200

    created TopStrategy file and created logic but not yet implemented

---
+++ src/main/java/nz/ac/auckland/se281/TopStategy.java
@@ -0,0 +1,49 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Main.Choice;
+
+public class TopStategy implements Strategy{
+
+
+  private static int sum;
+  private Choice choice;
+
+  public static void addFingers(int fingers) {
+    boolean even = Utils.isEven(fingers);
+    if ( even == true){
+      sum += 1;
+    } else if( even == false){
+      sum -= 1;
+    }
+  }
+
+  public void setChoice(Choice choice) {
+    this.choice = choice;
+  }
+
+  public static void clear(){
+    sum = 0;
+  }
+
+
+  @Override
+  public String selectFingers() {
+    if (choice == Choice.EVEN){
+      if (sum > 0){
+        return String.valueOf(Utils.getRandomOddNumber());
+      } else if (sum < 0){
+        return String.valueOf(Utils.getRandomEvenNumber());
+      }
+    } else if(choice == Choice.ODD){
+      if (sum > 0){
+        return String.valueOf(Utils.getRandomEvenNumber());
+      } else if (sum < 0){
+        return String.valueOf(Utils.getRandomOddNumber());
+      }
+    }
+    
+    
+    return String.valueOf(Utils.random.nextInt(6));
+  }
+  
+}
